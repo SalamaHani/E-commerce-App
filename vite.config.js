91c672d801d211824d64604ff20fa0fd50534d13
Salama <122737355+SalamaHani@users.noreply.github.com>
2025-07-01 12:10:16 +0300

    push base

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/E-commerce-App ",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"), // <-- here!
